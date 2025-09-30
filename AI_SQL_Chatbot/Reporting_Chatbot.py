a818631841755fac733d8a5f9ea9409b8b303c78
Matt Nowe <matt.nowe@gmail.com>
2025-09-29 21:54:46 -0400

    remove password field

--- AI_SQL_Chatbot/Reporting_Chatbot.py
+++ AI_SQL_Chatbot/Reporting_Chatbot.py
@@ -11,7 +11,6 @@ from openai import AzureOpenAI
 # Database connection configuration
 config = {
     'user': 'root',
-    'password': '',
     'host': 'localhost',
     'port': 3306,
     'database':'Main'
