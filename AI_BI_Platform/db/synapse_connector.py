a962e295da0cf2996b8a0cbcdd1e023e75700cf9
Matt Nowe <matt.nowe@gmail.com>
2025-10-07 15:04:10 -0400

    fix conflict

--- AI_BI_Platform/db/synapse_connector.py
+++ AI_BI_Platform/db/synapse_connector.py
@@ -1,18 +1,7 @@
 import pyodbc
 from config import CONN_STR
 
-<<<<<<< Updated upstream
-def get_connection():
-    """Establish a connection to Azure Synapse."""
-    try:
-        conn = pyodbc.connect(CONN_STR)
-        print("✅ Connected to Azure Synapse")
-        return conn
-    except Exception as e:
-        print(f"❌ Connection failed: {e}")
-        return None
-get_connection()
-=======
+
 def get_db_connection():
     """
     Gets the DB connection to be used throughout the app
@@ -22,6 +11,5 @@ def get_db_connection():
         print("Connected To Database")
         return conn
     except Exception as e:
-        print("Unable to Connect")
+        print("Unable to Connect:\n{e}")
         return None 
->>>>>>> Stashed changes
