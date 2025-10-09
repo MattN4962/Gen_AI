bb134dc6ff42ee05c55b914523da94dc4cce6437
Matthew <Matthew-Nowe@gnc-hq.com>
2025-10-09 10:57:56 -0400

    Update prompt for model

--- AI_BI_Platform/ai/agents.py
+++ AI_BI_Platform/ai/agents.py
@@ -10,12 +10,23 @@ class SQL_Agent:
     def __init__(self, client):
         self.client = client
     
-    def generate_sql(self, user_query):
+    def generate_sql(self, user_query, schema):
         response = self.client.chat.completions.create(
             model=DEFAULT_MODEL,
             messages=[
                 {
-                    "role": "system", "content": f"You are a SQL exper"
+                    "role":"system",
+                "content":f"""You will take the user query in natural language and produce a a SINGLE SQL query using only SELECT statements using SQL Server query syntax."
+                " This is the table schema: {schema}, and the field is_Pro is either True or False."
+                "When placing brackets around schema and table names, make sure that ONLY table and schema names are in []'s" 
+                "Always ensure joins and filters compare columns of the same data type. "
+                "If joining numeric and string columns, use TRY_CAST on both sides to avoid conversion errors."
+                "If a column has a datetimeoffset data type, always CAST it as DATETIME in the query "
+                "to ensure compatibility with pyodbc and Python."
+                "Only use columns that you know exist in the provided schema. Do NOT assume and columns exist if they are not listed"  
+                """
                 }
             ]
+            temperature=0
         )
+        return response.choices[0].message.content.strip()
